76095d2b164fc56dd18477d2395f36654e27b9a8
SilviuMihnea <40487014+SilviuMihnea@users.noreply.github.com>
2020-05-13 15:44:03 +0300

    First commit

---
+++ src/test/java/org/chimerax/hades/HadesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chimerax.hades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HadesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
