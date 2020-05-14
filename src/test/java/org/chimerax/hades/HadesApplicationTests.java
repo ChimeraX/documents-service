1cef8a5d352a60e4aaf72ad8c9d2b80b89a5052b
SilviuMihnea <40487014+SilviuMihnea@users.noreply.github.com>
2020-05-14 21:33:16 +0300

    Gradle setup

--- src/test/java/org/chimerax/hades/HadesApplicationTests.java
+++ src/test/java/org/chimerax/hades/HadesApplicationTests.java
@@ -2,8 +2,12 @@ package org.chimerax.hades;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@TestPropertySource("classpath:application-test.properties")
 class HadesApplicationTests {
 
     @Test
